87b31634b78b342a1c51e2aecddd3dd8111a7182
StavHayounNoiberg <stavh@me.com>
2024-05-29 21:51:09 +0300

    removed commented code

--- simulation_runner/simulation_manager.py
+++ simulation_runner/simulation_manager.py
@@ -2,13 +2,17 @@ from datetime import datetime, timedelta
 import heapq
 import logging
 import numpy as np
-from FinalProjectSimulator.data_repo.timeseries import fetch_timeseries_data_by_primary_key
+from FinalProjectSimulator.data_repo.timeseries import (
+    fetch_timeseries_data_by_primary_key,
+)
 from FinalProjectSimulator.data_repo.ridership import fetch_stations_passengers_by_day
 from FinalProjectSimulator.models.simulation import Simulation
 from FinalProjectSimulator.simulation_runner.line_manager import LineManager
 from FinalProjectSimulator.simulation_runner.package_models.event import Event
 from FinalProjectSimulator.simulation_runner.route_manager import RouteManager
-from FinalProjectSimulator.utilities.distributions import create_datetimes_poisson_distribution
+from FinalProjectSimulator.utilities.distributions import (
+    create_datetimes_poisson_distribution,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -76,7 +80,7 @@ class SimulationManager:
 
         logger.debug("finished")
         return True
-    
+
     def __create_passengers_events(self) -> list["PassengerRequest"]:
         logger.debug("started")
         logger.info("Creating passengers events")
@@ -147,7 +151,10 @@ class SimulationManager:
                 if stop.ordinal_number > src_station.ordinal_number
             ]
             if not optional_dsts:
-                logger.warning("No optional destinations for station %s, probably it is the last stop", src_station.name)
+                logger.warning(
+                    "No optional destinations for station %s, probably it is the last stop",
+                    src_station.name,
+                )
                 continue
             dst_station = np.random.choice(optional_dsts)
             is_reporting = np.random.choice(
@@ -172,100 +179,6 @@ class SimulationManager:
 
         logger.debug("finished")
         return events
-    
-    
+
+
 from FinalProjectSimulator.simulation_runner.package_models.passenger_request import PassengerRequest
-
-    # def create_events(
-    #     self,
-    #     event_times_distribution: DistributionTypes,
-    #     reporting_rate: float,
-    #     param,
-    #     start_time: datetime,
-    #     end_time: datetime,
-    # ) -> list[Event]:
-    #     if reporting_rate > 1:
-    #         raise ValueError("future requests rate can not be greater than 1 (100%)")
-
-    #     if event_times_distribution == DistributionTypes.POISSON:
-    #         event_times = Distributions.create_event_times_poisson(
-    #             param, start_time, end_time
-    #         )
-    #     elif event_times_distribution == DistributionTypes.RANDOM:
-    #         event_times = Distributions.create_event_times_random(
-    #             param, start_time, end_time
-    #         )
-    #     elif event_times_distribution == DistributionTypes.EMPIRIC:
-    #         event_times = Distributions.create_event_times_empiric(
-    #             param, start_time, end_time
-    #         )
-
-    #     requests_types_distribution = choices(
-    #         [EventType.FUTURE_REQUEST, EventType.IMMEDIATE_REQUEST],
-    #         [reporting_rate, 1 - reporting_rate],
-    #         k=len(event_times),
-    #     )
-
-    #     for event_type, event_time in zip(requests_types_distribution, event_times):
-    #         self.queue.append(
-    #             Event(
-    #                 event_time,
-    #                 event_type,
-    #                 # TODO: Add all parameters of Event
-    #                 "line_id",
-    #                 "stop_src",
-    #                 "stop_dest",
-    #                 event_time + timedelta(hours=1),
-    #             )
-    #         )
-
-    #     # Calculate the total duration
-    #     total_duration_hours = (
-    #         end_time - start_time
-    #     ).total_seconds() / 3600  # in hours
-
-    #     for hour in range(int(total_duration_hours)):
-    #         event_time = (start_time + timedelta(hours=hour)).replace(microsecond=0)
-    #         self.queue.append(Event(event_time, EventType.FUTUTRE_REQUESTS_CALC))
-
-    #     heapq.heapify(self.queue)
-    #     return self.queue
-
-    # def count_events_by_type(self):
-    #     immediate_count = sum(
-    #         1 for event in self.queue if event.get_type() == EventType.IMMEDIATE_REQUEST
-    #     )
-    #     future_count = sum(
-    #         1 for event in self.queue if event.get_type() == EventType.FUTURE_REQUEST
-    #     )
-    #     calc_count = sum(
-    #         1
-    #         for event in self.queue
-    #         if event.get_type() == EventType.FUTUTRE_REQUESTS_CALC
-    #     )
-    #     return immediate_count, future_count, calc_count
-
-    # def get_queue_len(self) -> int:
-    #     return len(self.queue)
-
-
-# events_manager = EventsManager()
-# events_manager.create_events(
-#     DistributionTypes.POISSON,
-#     0.2,
-#     10,
-#     datetime.now(),
-#     datetime.now() + timedelta(hours=24),
-# )
-
-# immediate_count, future_count, calc_count = events_manager.count_events_by_type()
-# request_events = immediate_count + future_count
-# print("Total events:", events_manager.get_queue_len())
-# print("Request events:", request_events)
-# print("Immediate events:", immediate_count, "Ratio:", round(immediate_count / request_events, 2))
-# print("Future events:", future_count, "Ratio:", round(future_count / request_events, 2))
-# print("Calculation events:", calc_count)
-# print("First event is:", events_manager.peek_next_event())
-# first_event = events_manager.pop_next_event()
-# print("Event taken out:", first_event)
-# print("Next event is:", events_manager.peek_next_event())
