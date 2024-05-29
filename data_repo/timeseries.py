53e172474c6124ddc3d2363e8f4bac5d29afec11
StavHayounNoiberg <stavh@me.com>
2024-05-29 21:59:52 +0300

    fix formating

--- data_repo/timeseries.py
+++ data_repo/timeseries.py
@@ -6,7 +6,9 @@ from FinalProjectSimulator.data_repo.db_pool import get_timeseries_con
 logger = logging.getLogger(__name__)
 
 
-def fetch_timeseries_data_by_primary_key(full_line_id: str, day: int, data_type: str) -> pd.DataFrame | None:
+def fetch_timeseries_data_by_primary_key(
+    full_line_id: str, day: int, data_type: str
+) -> pd.DataFrame | None:
     conn = get_timeseries_con()
     try:
         line_parts = full_line_id.split("-")
