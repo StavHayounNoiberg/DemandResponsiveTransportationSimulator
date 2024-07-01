59b8b831aaa60a447ea1ad228de2bcabd44376f3
StavHayounNoiberg <stavh@me.com>
2024-07-01 20:32:38 +0300

    Added iterations on different threads

--- simulation_runner/__main__.py
+++ simulation_runner/__main__.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 import os
 import sys
+import threading
 from FinalProjectSimulator.models.simulation import Simulation
 from FinalProjectSimulator.simulation_runner.logging_config import setup_logging
 from FinalProjectSimulator.simulation_runner.simulation_manager import SimulationManager
@@ -15,43 +16,59 @@ setup_logging(log_dir, log_level=20)  # DEBUG level (10)
 logger = logging.getLogger(__name__)
 
 
+def run_simulation(simulation_id, line_id, start_time, end_time, express_rate, reporting_rate, simulation_start_time):
+    # Create an instance of Simulation and SimulationManager and create the events
+    simulation = Simulation(
+        simulation_id,
+        line_id,
+        start_time,
+        end_time,
+        express_rate,
+        reporting_rate,
+        simulation_start_time,
+    )
+    manager = SimulationManager(simulation)
+    manager.create_events()
+
+    logger.info("Simulation %s started", simulation_id)
+    # Iterate over the events and handle them
+    while manager.peek_next_event() is not None:
+        event = manager.pop_next_event()
+        event.handle()
+        event.mark_handled()
+
+    # Save the simulation results
+    if manager.save_results() is True:
+        logger.info("Simulation results saved successfully")
+    else:
+        logger.error("Simulation results were not saved successfully")
+
+
 if __name__ == "__main__":
     # Get parameters sent from the command line (simulation_id, line_id, start_time, end_time, express_rate, reporting_rate, line_id)
     try:
         logger.info("Simulation starting")
         simulation_start_time = datetime.now()
-        if len(sys.argv) >= 6:
+        if len(sys.argv) == 8:
             simulation_id = sys.argv[1]
             line_id = sys.argv[2]
             start_time = datetime.strptime(sys.argv[3], "%d-%m-%Y.%H:%M")
             end_time = datetime.strptime(sys.argv[4], "%d-%m-%Y.%H:%M")
             express_rate = float(sys.argv[5])
             reporting_rate = float(sys.argv[6])
-
-            # Create an instance of Simulation and SimulationManager and create the events
-            simulation = Simulation(
-                simulation_id,
-                line_id,
-                start_time,
-                end_time,
-                express_rate,
-                reporting_rate,
-                simulation_start_time,
-            )
-            manager = SimulationManager(simulation)
-            manager.create_events()
-
-            logger.info("Simulation started")
-            # Iterate over the events and handle them
-            while manager.peek_next_event() is not None:
-                event = manager.pop_next_event()
-                event.handle()
-
-            # Save the simulation results
-            if manager.save_results() is True:
-                logger.info("Simulation results saved successfully")
-            else:
-                logger.error("Simulation results were not saved successfully")
+            iterations = int(sys.argv[7])
+            
+            threads = []
+            for i in range(iterations):
+                iteration_id = f"{simulation_id}/{i}"
+                thread_name = f"Iteration {i}"
+                thread = threading.Thread(target=run_simulation, name=thread_name, args=(iteration_id, line_id, start_time, end_time, express_rate, reporting_rate, simulation_start_time))
+                threads.append(thread)
+                thread.start()
+            
+            for thread in threads:
+                thread.join()
+                
         else:
             logger.error("Missing parameters")
     except Exception as e:
