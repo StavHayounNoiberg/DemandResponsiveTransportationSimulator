ac8d1f5541aec863bd26bed8f084fad9e99edfa0
StavHayounNoiberg <stavh@me.com>
2024-07-01 21:39:07 +0300

    Added thread batching

--- simulation_runner/__main__.py
+++ simulation_runner/__main__.py
@@ -8,6 +8,9 @@ from FinalProjectSimulator.simulation_runner.logging_config import setup_logging
 from FinalProjectSimulator.simulation_runner.simulation_manager import SimulationManager
 
 
+BATCH_SIZE = os.cpu_count()
+
+
 # Set up logging
 root_dir = os.path.dirname(os.path.abspath(__file__))
 log_dir = os.path.join(root_dir, 'logs')
@@ -16,8 +19,9 @@ setup_logging(log_dir, log_level=20)  # DEBUG level (10)
 logger = logging.getLogger(__name__)
 
 
-def run_simulation(simulation_id, line_id, start_time, end_time, express_rate, reporting_rate, simulation_start_time):
+def run_simulation(simulation_id, line_id, start_time, end_time, express_rate, reporting_rate):
     # Create an instance of Simulation and SimulationManager and create the events
+    simulation_start_time = datetime.now()
     simulation = Simulation(
         simulation_id,
         line_id,
@@ -48,7 +52,6 @@ if __name__ == "__main__":
     # Get parameters sent from the command line (simulation_id, line_id, start_time, end_time, express_rate, reporting_rate, line_id)
     try:
         logger.info("Simulation starting")
-        simulation_start_time = datetime.now()
         if len(sys.argv) == 8:
             simulation_id = sys.argv[1]
             line_id = sys.argv[2]
@@ -62,12 +65,17 @@ if __name__ == "__main__":
             for i in range(iterations):
                 iteration_id = f"{simulation_id}/{i}"
                 thread_name = f"Iteration {i}"
-                thread = threading.Thread(target=run_simulation, name=thread_name, args=(iteration_id, line_id, start_time, end_time, express_rate, reporting_rate, simulation_start_time))
+                thread = threading.Thread(target=run_simulation, name=thread_name, args=(iteration_id, line_id, start_time, end_time, express_rate, reporting_rate))
                 threads.append(thread)
-                thread.start()
+                
+                if len(threads) == BATCH_SIZE or i == iterations - 1:
+                    for thread in threads:
+                        thread.start()
             
-            for thread in threads:
-                thread.join()
+                    for thread in threads:
+                        thread.join()
+
+                    threads = []
                 
         else:
             logger.error("Missing parameters")
