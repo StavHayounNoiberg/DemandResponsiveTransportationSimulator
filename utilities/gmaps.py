d8432536e6901b8373c59ee3258d646080be470b
StavHayounNoiberg <stavh@me.com>
2024-07-19 11:25:03 +0300

    Change uasge of gmaps

--- utilities/gmaps.py
+++ utilities/gmaps.py
@@ -13,27 +13,34 @@ API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY'
 gmaps = googlemaps.Client(key=API_KEY)
 
 
-def __get_route_timedeltas(leave_time: datetime, waypoints: list[tuple[float, float]], mode="driving") -> list[timedelta]:
-
+def __get_route_timedeltas(waypoints: list[tuple[float, float]], mode = "driving", leave_time: datetime = datetime.min) -> list[timedelta]:
+    
     timedeltas: list[timedelta] = [timedelta(seconds=0)]
-
-    departure_time = leave_time
-    if departure_time < datetime.now():
-        diff = datetime.now() - departure_time
-        weeks_to_add = (diff.days // 7) + 1
-        departure_time += timedelta(weeks=weeks_to_add)
 
     start_coords = waypoints[0]
     end_coords = waypoints[-1]
     waypoints = waypoints[1:-1]
 
     try:
-        # Request public transport directions with waypoints
-        directions_result = gmaps.directions(start_coords,
-                                             end_coords,
-                                             mode=mode,
-                                             waypoints=waypoints,
-                                             departure_time=departure_time)
+        directions_result = None
+        
+        if leave_time > datetime.min:
+            departure_time = leave_time
+            if departure_time < datetime.now():
+                diff = datetime.now() - departure_time
+                weeks_to_add = (diff.days // 7) + 1
+                departure_time += timedelta(weeks=weeks_to_add)
+                
+            directions_result = gmaps.directions(start_coords,
+                                                end_coords,
+                                                mode=mode,
+                                                waypoints=waypoints,
+                                                departure_time=departure_time)
+        else:
+            directions_result = gmaps.directions(start_coords,
+                                                end_coords,
+                                                mode=mode,
+                                                waypoints=waypoints)
 
         # Extract and print the route length, duration, and time of arrival at each stop
         if directions_result:
@@ -67,7 +74,7 @@ def get_route(leave_time: datetime, waypoints: list[tuple[float, float]], mode="
             segment_waypoints = waypoints[i:i+25]
         else:
             segment_waypoints = waypoints[i-1:i+25]
-        timedeltas = __get_route_timedeltas(last_time, segment_waypoints, mode)
+        timedeltas = __get_route_timedeltas(segment_waypoints, mode)
         if timedeltas is None:
             return None
         
@@ -79,13 +86,12 @@ def get_route(leave_time: datetime, waypoints: list[tuple[float, float]], mode="
     return datetimes
 
 
-def get_route_timedeltas(leave_time: datetime, waypoints: list[tuple[float, float]], mode="driving") -> list[timedelta]:
+def get_route_timedeltas(waypoints: list[tuple[float, float]], mode="driving") -> list[timedelta]:
     if len(waypoints) < 2:
         logger.error("Not enough waypoints provided")
         return None
 
     timedeltas: list[timedelta] = []
-    last_time = leave_time
 
     for i in range(0, len(waypoints), 25):
         segment_waypoints = []
@@ -93,11 +99,10 @@ def get_route_timedeltas(leave_time: datetime, waypoints: list[tuple[float, floa
             segment_waypoints = waypoints[i:i+25]
         else:
             segment_waypoints = waypoints[i-1:i+25]
-        route_timedeltas = __get_route_timedeltas(last_time, segment_waypoints, mode)
+        route_timedeltas = __get_route_timedeltas(segment_waypoints, mode)
         if route_timedeltas is None:
             return None
         
-        last_time += route_timedeltas[-1]
         if i > 0:
             route_timedeltas = [time + timedeltas[-1] for time in route_timedeltas[1:]]
         timedeltas.extend(route_timedeltas)        
